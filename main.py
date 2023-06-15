fcf8be79362e4fdd689b3a1d3bc5351cefe81015
Mitchell Parker <mitch.isaac.parker@gmail.com>
2023-06-15 09:58:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ app = FastAPI()
 # Decorate Function
 @app.get("/")
 def run(age: int = 65, female: bool = True, chf: bool = False, hypertension: bool = False, stroke_tia: bool = False, vascular_disease: bool = False, diabetes: bool = False) -> int:
-    return chads_vasc_score(age=age, 
+    score =  chads_vasc_score(age=age, 
                     female=female, 
                     chf=chf, 
                     hypertension=hypertension,
                     stroke_tia=stroke_tia, 
                     vascular_disease=vascular_disease, 
                     diabetes=diabetes)
+    return f"CHA₂DS₂-VASc Score: {score}"
