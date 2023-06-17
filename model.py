c9cdd9ae41d0673d1c453f8652023f28d62bb82b
Mitchell Parker <mitch.isaac.parker@gmail.com>
2023-06-16 22:19:30 -0400

    Update model.py

--- model.py
+++ model.py
@@ -44,18 +44,3 @@ def chads_vasc_score(age: int, female: bool, chf: bool, hypertension: bool, stro
         score += 1
 
     return score
-
-
-template = """
-            Task: Determine if anticoagulation is recommended based on the patient's CHA2DS2-VASc score, sex, and context.
-
-            CHA2DS2-VASc Score: {score}
-
-            Sex: {sex}
-
-            Context: Most guidelines suggest that scores of 0 (men) or 1 (women) do not require treatment; however, all other patients should receive anticoagulation, preferably with a direct oral anticoagulant (unless contraindicated).
-            Anticoagulation is not recommended in patients with non-valvular AF and a CHA2DS2-VASc score of 0 if male or 1 if female, as these patients had no TE events in the original study.
-            Depending on a patient\'s preferences and individual risk factors, anticoagulation can be considered for a CHA2DS2-VASc score of 1 in males and 2 in females.
-            Anticoagulation should be started in patients with a CHA2DS2-VASc score of >2 if male or >3 if female.
-
-            Note: State if anticoagulation is recommended and nothing else. Only use the information from the context in your determination. Don't add any additional information. Limit your response to one sentence."""
